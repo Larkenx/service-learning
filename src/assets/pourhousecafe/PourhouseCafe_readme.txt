12fae4faa026bfbee8f42db26412d49f0d6bfe1c
larkenx <larkenx@gmail.com>
2017-09-22 22:19:49 -0400

    Automated commit

---
+++ src/assets/pourhousecafe/PourhouseCafe_readme.txt
@@ -0,0 +1,8 @@
+Pourhouse Cafe:
+
+-Clean website with muted colors
+-Emphasize missions and non profit missions
+-Emphasize donations that fund PHC
+-Updated pictures of the café 
+
+
--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,19 +1,28 @@
 <template>
         <!--  Opening Parallax Section -->
-        <v-card flat style="top: -50px;">
-            <v-card-media src="src/assets/DSC_0040.JPG" height="900px">
-            </v-card-media>
-        </v-card>
-
+        <!--<v-card flat style="top: -50px;">-->
+            <!--<v-card-media src="src/assets/DSC_0040.jpeg" height="1000px"></v-card-media>-->
+        <!--</v-card>-->
+        <v-carousel hide-controls>
+            <v-carousel-item v-for="(pic, i) in carouselPictures" key="i" :src="pic">
+            </v-carousel-item>
+        </v-carousel>
 </template>
 
 <script>
     import VParallax from "../../node_modules/vuetify/src/components/VParallax/VParallax";
+    import VCarousel from "../../node_modules/vuetify/src/components/VCarousel/VCarousel";
+    import VCarouselItem from "vuetify/src/components/VCarousel/VCarouselItem";
     /* import other components here */
     export default {
-        components: {VParallax},
+        components: {
+            VCarouselItem,
+            VCarousel,
+            VParallax},
         data() {
-            return {};
+            return {
+                carouselPictures : ["src/assets/DSC_0040.jpeg", "src/assets/DSC_0024.jpg", "src/assets/DSC_0041.jpg"]
+            };
         },
         methods: {},
     }
