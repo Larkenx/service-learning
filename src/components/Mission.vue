9d90a934d863bdd50673f7bd9e64b7b7358b4894
larkenx <larkenx@gmail.com>
2017-10-17 15:36:47 -0400

    Automated commit

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -1,36 +1,140 @@
 <template>
     <main>
+
         <section>
-            <v-parallax src="public/mission.jpg" height="600">
+            <v-parallax src="public/mission2.jpg" height="700">
                 <v-layout column
                           align-center
                           justify-center
-                          class="white--text">
-                    <p class="display-3">ONE CUP AT A TIME</p>
-                    <p class="subheading" style="text-align: center; width: 300px;">
-                        <i>
-                            We're making a difference all over the world one coffee cup at a time. See our current and previous
-                            missions below.
-                        </i>
-                    </p>
-                    <v-btn class="white--text" style="width: 150px; letter-spacing: 1px" outline to="/donate" exact>
-                        Learn more
-                    </v-btn>
+                          class="white--text parallax-text">
+                    <v-layout row align-center justify-center>
+                        <v-flex xs2>
+                            <span style="font-size: 120px; padding-right: 10px;">$</span>
+                        </v-flex>
+                        <v-flex xs10>
+                            <i-odometer class="iOdemeter" :value="donated"></i-odometer>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row align-bottom>
+                        <p class="display-1" style="text-align: center; width: 500px;">
+                            <i>
+                                As a community, we can make a difference. All of our tips shown above are
+                                donated to the following charities.
+                            </i>
+                        </p>
+                    </v-layout>
+
+                    <!--<p class="display-3">MORE THAN A CUP OF A COFFEE</p>-->
+
                 </v-layout>
             </v-parallax>
         </section>
+        <torn-border location="top"></torn-border>
+        <section style="padding: 20px;">
+            <v-layout
+                    column
+                    wrap
+                    align-center
+            >
+                <v-flex xs12>
+                    <!--<v-container grid-list-xs>-->
+                        <v-layout row wrap>
+                            <v-flex xs12 md4>
+
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">
+                                            Delicious coffee
+                                        </div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <!-- start slipsum code -->
+
+                                        The lysine contingency - it's intended to prevent the spread of the animals is case they ever got off the island. Dr. Wu inserted a gene that makes a single faulty enzyme in protein metabolism. The animals can't manufacture the amino acid lysine. Unless they're continually supplied with lysine by us, they'll slip into a coma and die.
+
+                                        <!-- end slipsum code -->
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">
+                                            For a good cause
+                                        </div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <!-- start slipsum code -->
+
+                                        You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.
+
+                                        <!-- end slipsum code -->
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    <!--</v-container>-->
+                </v-flex>
+            </v-layout>
+        </section>
+        <torn-border location="bottom"></torn-border>
+        <section>
+            <v-parallax src="public/mission2.jpg" height="700">
+                <v-layout column
+                          align-center
+                          justify-center
+                          class="white--text parallax-text">
+                    <p class="display-3">MORE THAN A CUP OF A COFFEE</p>
+                    <p class="subheading" style="text-align: center; width: 300px;">
+                        <i>
+                            We're making a difference all over the world one coffee cup at a time. All tips
+                            that we receive are donated to charitable missions all over the world.
+                        </i>
+                    </p>
+                </v-layout>
+            </v-parallax>
+        </section>
+
     </main>
 </template>
 
 <script>
     /* import other components here */
+    import TornBorder from './TornBorder.vue'
+    import IOdometer from "../../node_modules/vue-odometer/src/iOdometer";
     export default {
+        components: {
+            IOdometer,
+            TornBorder
+        },
         data() {
-            return {};
+            return {
+                donated : 1434
+            };
         },
         methods: {},
+        created () {
+//            window.odometerOptions = {
+//                auto: true, // Don't automatically initialize everything with class 'odometer'
+//                format: '(,ddd).dd', // Change how digit groups are formatted, and how many digits are shown after the decimal point
+//                duration: 3000, // Change how long the javascript expects the CSS animation to take
+//                theme: 'default', // Specify the theme (if you have more than one theme css file on the page)
+//                animation: 'count' // Count is a simpler animation method which just increments the value,
+//                                   // use it when you're looking for something more subtle.
+//            };
+            setInterval(() => {
+                this.donated += Math.round(Math.random() * 20 + 5);
+                console.log(this.donated);
+            }, 3000)
+        }
     }
 
 </script>
 <style>
+    .iOdemeter {
+        text-align: center;
+        font-size: 120px;
+    }
 </style>
--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -6,7 +6,7 @@
                 <v-layout column
                           align-center
                           justify-center
-                          class="white--text">
+                          class="white--text parallax-text">
                     <p class="display-3">COFFEE AND KINDNESS</p>
                     <p class="subheading" style="text-align: center; width: 300px;">
                         <i>
@@ -14,7 +14,8 @@
                             Come have a cup of coffee and donate to a good cause!
                         </i>
                     </p>
-                    <v-btn class="white--text" style="width: 150px; letter-spacing: 1px" outline to="/donate" exact>
+                    <v-btn class="white--text" style="width: 150px; letter-spacing: 1px" outline
+                           to="/donate" exact>
                         Learn more
                     </v-btn>
                 </v-layout>
@@ -22,79 +23,79 @@
         </section>
         <torn-border location="top"></torn-border>
         <section style="padding: 20px;">
-                <v-layout
-                        column
-                        wrap
-                        align-center
-                >
-                    <v-flex xs12 sm4>
-                        <div >
-                            <h3 class="coffee--text">Coffee on Kirkwood</h3>
-                        </div>
-                    </v-flex>
-                    <v-flex xs12>
-                        <v-container grid-list-xl>
-                            <v-layout row wrap align-top>
+            <v-layout
+                    column
+                    wrap
+                    align-center
+            >
+                <v-flex xs12 sm4>
+                    <div>
+                        <h3 class="coffee--text">Coffee on Kirkwood</h3>
+                    </div>
+                </v-flex>
+                <v-flex xs12>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap align-top>
 
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-media src="public/cover2.jpg" height="300px">
-                                        </v-card-media>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">
-                                                A cozy, quiet environment
-                                            </div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            <!-- start slipsum code -->
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-media src="public/cover2.jpg" height="300px">
+                                    </v-card-media>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">
+                                            A cozy, quiet environment
+                                        </div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <!-- start slipsum code -->
 
-                                            Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.
+                                        Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.
 
-                                            <!-- end slipsum code -->
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
+                                        <!-- end slipsum code -->
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
 
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-media src="public/DSC_0023.jpg" height="300px">
-                                        </v-card-media>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">
-                                                Delicious coffee
-                                            </div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            <!-- start slipsum code -->
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-media src="public/DSC_0023.jpg" height="300px">
+                                    </v-card-media>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">
+                                            Delicious coffee
+                                        </div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <!-- start slipsum code -->
 
-                                            The lysine contingency - it's intended to prevent the spread of the animals is case they ever got off the island. Dr. Wu inserted a gene that makes a single faulty enzyme in protein metabolism. The animals can't manufacture the amino acid lysine. Unless they're continually supplied with lysine by us, they'll slip into a coma and die.
+                                        The lysine contingency - it's intended to prevent the spread of the animals is case they ever got off the island. Dr. Wu inserted a gene that makes a single faulty enzyme in protein metabolism. The animals can't manufacture the amino acid lysine. Unless they're continually supplied with lysine by us, they'll slip into a coma and die.
 
-                                            <!-- end slipsum code -->
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-media src="public/cover4.jpg" height="300px">
-                                        </v-card-media>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">
-                                                For a good cause
-                                            </div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            <!-- start slipsum code -->
+                                        <!-- end slipsum code -->
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-media src="public/cover4.jpg" height="300px">
+                                    </v-card-media>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">
+                                            For a good cause
+                                        </div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <!-- start slipsum code -->
 
-                                            You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.
+                                        You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.
 
-                                            <!-- end slipsum code -->
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-flex>
-                </v-layout>
+                                        <!-- end slipsum code -->
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+            </v-layout>
         </section>
         <torn-border location="bottom"></torn-border>
         <section>
@@ -102,7 +103,7 @@
                 <v-layout column
                           align-center
                           justify-center
-                          class="white--text">
+                          class="white--text parallax-text">
                     <p class="display-3">COME AND VISIT</p>
                     <p class="subheading" style="text-align: center; width: 300px;">
                         <i>
@@ -110,14 +111,15 @@
                             Come have a cup of coffee and donate to a good cause!
                         </i>
                     </p>
-                    <v-btn class="white--text" style="width: 200px; letter-spacing: 1px" outline to="/donate" exact>
+                    <v-btn class="white--text" style="width: 200px; letter-spacing: 1px" outline
+                           target="_blank"
+                           href="https://www.google.com/maps/dir/''/the+pourhouse+cafe/@39.1663208,-86.5308209,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x886c66dd474de711:0xf6fec7665cceb229!2m2!1d-86.5302716!2d39.1663418"
+                           exact>
                         Find us on google maps
                     </v-btn>
                 </v-layout>
             </v-parallax>
         </section>
-
-
     </main>
 </template>
 
